2c4581f5b2922908e67b2d178b1ae73c8b29b2e8
wathika-eng <wathika02@gmail.com>
2025-10-17 18:41:14 +0300

    Add .gitignore and initial index.php for API response handling

---
+++ index.php
@@ -0,0 +1,52 @@
+<?
+
+// set json response header
+header("Content-Type: application/json");
+
+// redirect to me endpoint
+$request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+if ($request_uri !== '/me') {
+    header("Location: /me", true, 301);
+    exit;
+}
+
+$url = "https://catfact.ninja/fact";
+
+
+// init curl
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $url);
+// receive response
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+
+// handle error
+if (!$response) {
+    http_response_code(500);
+    echo json_encode([
+        "status" => "error",
+        "message" => curl_error($ch)
+    ]);
+    exit;
+} else {
+    // decode data
+    $factData = json_decode($response, true);
+
+}
+
+curl_close($ch);
+
+
+$response_body = [
+    "status" => "success",
+    "user" => [
+        "email" => "wathika02@gmail.com",
+        "name" => "Joseph Wathika",
+        "stack" => "PHP"
+    ],
+    "timestamp" => gmdate(("c")),
+    "fact" => $factData["fact"] ?? "No fact found"
+];
+
+echo (json_encode($response_body, JSON_PRETTY_PRINT));
